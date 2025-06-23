7cd79cd4b0b3b3578c738f88624a79b42dec51b7
TermPinsa <termsak01@gmail.com>
2025-06-23 10:57:36 +0700

    third commit

---
+++ Job2_6_2n40.c
@@ -0,0 +1,15 @@
+//เขียนโปรแกรมรับข้อมูลจำนวนเต็ม 5 จำนวนจากผู้ใช้ และหาว่าค่าเฉลี่ยของข้อมูลที่รับเข้ามามีค่าเท่าใด
+#include<stdio.h>
+void main()
+{
+    int i,count=5,num,sum=0;
+    float av=0;
+    for (i = 1; i <= count; i++)
+    {
+        printf("Enter number %d : ",i);
+        scanf("%d",&num);
+        sum=sum+num;
+    }
+    av=sum/count;
+    printf("Average : %.2f",av);
+}
