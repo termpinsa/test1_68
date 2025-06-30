f42b12ce29b11658bd6f9eea3a3c8ac7a501e4ed
TermPinsa <termsak01@gmail.com>
2025-06-30 10:10:03 +0700

    main1

---
+++ Job3_11n72.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    float P_price,Vat=0,T_price=0;
+    printf("Enter Product price : ");
+    scanf("%f",&P_price);
+    Vat=P_price*0.07;
+    T_price=P_price+Vat;
+    printf("Vat = %.2f",Vat);
+    printf("\nTotal price = %.2f",T_price);
+
+
+}
